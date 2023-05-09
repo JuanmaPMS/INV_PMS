cf277f71b94649f8b7d771c3bdc87fdc06215528
mitzihdz <mitzi.hernandez@people-media.com.mx>
2023-05-09 13:49:26 -0600

    Ajustes responsiva

--- Entidades_complejas/responsiva_complex.cs
+++ Entidades_complejas/responsiva_complex.cs
@@ -11,10 +11,14 @@ namespace Entidades_complejas
         public string? Usuario { get; set; }
         public string? Propietario { get; set; }
         public bool Asignacion { get; set; }
+        public string? User { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
         public List<info_equipo_complex>? InfoGeneralEquipo { get; set; } = new();
         public List<info_equipo_complex>? InfoAccesoriosEquipo { get; set; } = new();
         public List<string>? InfoHardwareEquipo { get; set; } = new();
         public List<string>? InfoSoftwareEquipo { get; set; }
+
     }
 
     public class info_equipo_complex
