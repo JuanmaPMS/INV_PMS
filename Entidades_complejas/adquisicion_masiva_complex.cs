26f589642941de78a15912740f4e0ee5608f6277
mitzihdz <mitzi.hernandez@people-media.com.mx>
2023-05-15 14:16:20 -0600

    Servicio para carga de excel

---
+++ Entidades_complejas/adquisicion_masiva_complex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_complejas
+{
+    public class adquisicion_masiva_complex
+    {
+        public string? Proveedor { get; set; }
+        public string? Propietario { get; set; }
+        public string? Monto { get; set; }
+        public string? Impuesto { get; set; }
+        public string? Articulos { get; set; }
+        public DateTime? Fechadecompra { get; set; }
+        public List<adquisicion_masiva_detalle_complex>? RelAdquisicionDetalles { get; set; }
+    }
+
+    public class adquisicion_masiva_detalle_complex
+    {
+        public string? Producto { get; set; }
+        public string? Costosiunitario { get; set; }
+        public string? Numerodeserie { get; set; }
+        public string? Inventarioclv { get; set; }
+    }
+}
--- Negocio/CargaMasiva/CargaMasivaNegocio.cs
+++ Negocio/CargaMasiva/CargaMasivaNegocio.cs
@@ -1,4 +1,5 @@
 ﻿using Data.Models;
+using Entidades_complejas;
 using Spire.Xls;
 using System;
 using System.Collections.Generic;
@@ -125,6 +126,7 @@ namespace Negocio.CargaMasiva
                 productosNuevos.Visibility = WorksheetVisibility.Hidden;
                 MemoryStream stream = new MemoryStream();
                 workbook.SaveToStream(stream);
+                //workbook.SaveToFile("Pantilla.xlsx", ExcelVersion.Version2010);
                 byte[] data = stream.ToArray();
                 stream.Flush();
                 stream.Close();
@@ -135,5 +137,114 @@ namespace Negocio.CargaMasiva
                 return null;
             }
         }
+
+        //Carga masiva
+        public TipoAccion carga_masiva(adquisicion_masiva_complex input)
+        {
+            TipoAccion result = new TipoAccion();
+            try
+            {
+                string MsError = string.Empty;
+                int ProveedorId = 0;
+                int PropietarioId = 0;
+
+                CatProveedor catProveedor = ctx.CatProveedors.Where(x => x.Razonsocial == input.Proveedor).FirstOrDefault();
+                if(catProveedor == null)
+                { MsError = "No se encontró al proveedor\n"; }
+                else { ProveedorId = catProveedor.Id; }
+
+                CatPropietario catPropietario = ctx.CatPropietarios.Where(x => x.Razonsocial == input.Propietario).FirstOrDefault();
+                if (catPropietario == null)
+                { MsError = "No se encontró al propietario\n"; }
+                else { PropietarioId = catPropietario.Id; }
+
+                TblAdquisicion ad = new TblAdquisicion();
+                ad.CatProveedorId = ProveedorId;
+                ad.CatPropietarioId = PropietarioId;
+                ad.Monto = Convert.ToDouble(input.Monto!);
+                ad.Impuesto = Convert.ToDouble(input.Impuesto!);
+                ad.Articulos = Convert.ToInt32(input.Articulos!);
+                ad.Fechadecompra = input.Fechadecompra!.Value;
+                ad.FacXml = string.Empty;
+                ad.FacPdf = string.Empty;
+                ctx.TblAdquisicions.Add(ad);
+                ctx.SaveChanges();
+
+                if (input.RelAdquisicionDetalles != null && input.RelAdquisicionDetalles.Count() > 0)
+                {
+                    foreach (adquisicion_masiva_detalle_complex detalle_complex in input.RelAdquisicionDetalles)
+                    {
+                        int ProductoId = 0;
+                        double CostoUnitario = 0;
+                        CatProducto catProducto = ctx.CatProductos.Where(x => x.Modelo == detalle_complex.Producto).FirstOrDefault();
+                        if (catProducto == null)
+                        { MsError = "No se encontró el producto: " + detalle_complex.Producto + "\n"; }
+                        else { ProductoId = catProducto.Id; }
+
+                        try { CostoUnitario = Convert.ToDouble(detalle_complex.Costosiunitario!); }
+                        catch { MsError = "El costo unitario del producto: " + detalle_complex.Producto + ", no tiene el formato requerido.\n"; }
+
+                        if(ProductoId > 0)
+                        {
+                            //Inserta inventario
+                            TblInventario tblInventario = new TblInventario();
+
+                            tblInventario.TblAdquisicionId = ad.Id;
+                            tblInventario.CatProductoId = ProductoId;
+                            tblInventario.CatEstatusinventarioId = 1;//LIBRE
+                            tblInventario.Numerodeserie = detalle_complex.Numerodeserie;
+                            tblInventario.Inventarioclv = detalle_complex.Inventarioclv;
+                            tblInventario.Estatus = true;
+                            tblInventario.Inclusion = DateTime.Now;
+
+                            ctx.TblInventarios.Add(tblInventario);
+                            ctx.SaveChanges();
+
+
+                            RelAdquisicionDetalle relDetalle = ctx.RelAdquisicionDetalles.Where(x => x.TblAdquisicionId == ad.Id && x.CatProductoId == ProductoId).FirstOrDefault();
+                            if (relDetalle == null)
+                            {
+                                RelAdquisicionDetalle adquisicionDetalle = new RelAdquisicionDetalle();
+                                adquisicionDetalle.Cantidad = 1;
+                                adquisicionDetalle.TblAdquisicionId = ad.Id;
+                                adquisicionDetalle.CatProductoId = ProductoId;
+                                adquisicionDetalle.Costosiunitario = CostoUnitario;
+                                adquisicionDetalle.Estatus = true;
+                                adquisicionDetalle.Inclusion = DateTime.Now;
+
+                                ctx.RelAdquisicionDetalles.Add(adquisicionDetalle);
+                                ctx.SaveChanges();
+                            }
+                            else
+                            {
+                                relDetalle.Cantidad = relDetalle.Cantidad + 1;
+                                ctx.RelAdquisicionDetalles.Update(relDetalle);
+                                ctx.SaveChanges();
+                            }
+                        }
+                        
+                    }
+                }
+
+                if(string.IsNullOrEmpty(MsError))
+                {
+                    result = TipoAccion.Positiva("Carga completada", ad.Id);
+                }
+                else
+                {
+                    result = TipoAccion.Negativa(MsError, ad.Id);
+                }          
+            }
+            catch (Exception ex)
+            {
+                result = TipoAccion.Negativa(ex.Message);
+            }
+
+            return result;
+        }
+
+
+
+
     }
 }
